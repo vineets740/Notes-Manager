3e62d650f2652350fec7145ffc8b975dc4ceb695
vineets740 <vineets740@gmail.com>
2019-02-10 13:13:43 +0530

    Application Completed

---
+++ app.js
@@ -0,0 +1,47 @@
+const fs=require('fs');
+const _ = require('lodash');
+const yargs = require('yargs');
+
+const notes = require('./notes.js');
+
+var argv = yargs.argv;
+var command = argv._[0];
+
+
+// console.log('Process', process.argv);
+// console.log('Yargs', argv);
+
+if(command == 'add'){
+    var note = notes.addNote(argv.title, argv.body);
+    if(note){
+        console.log('Note Created');
+        notes.logNote(note);
+    }else{
+        console.log("Note title taken");
+    }
+}else if(command === 'list'){
+    var allNotes = notes.getAll();
+    console.log(`Printing ${allNotes.length} note(s)`);
+    allNotes.forEach(note => notes.logNote(note));
+}else if(command === 'read'){
+    var note = notes.getNote(argv.title);
+    if(note){
+        console.log('Note Found');
+        notes.logNote(note);
+    }else{
+        console.log("Note Not Found");
+    }
+}else if(command === 'remove'){
+    var removed = notes.removeNote(argv.title);
+    if(removed){
+        console.log('Removed Note');
+        console.log('--');
+        console.log(`Title: ${argv.title}`);
+        console.log(`Body: ${argv.body}`);
+    }else{
+        console.log('Note Not Found');
+    }
+    
+}else{
+    console.log('Command not recognised');
+}
